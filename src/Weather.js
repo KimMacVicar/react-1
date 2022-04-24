42a816a98c82eca94fe1c072d5e1a99ea8d18c92
KimMacVicar <92276525+KimMacVicar@users.noreply.github.com>
2022-04-24 14:31:04 -0600

    changed color of loader

---
+++ src/Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import { Triangle } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "2d34b827fa05a5518d1b4d8e1f540549";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}$units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Triangle
+      type="Puff"
+      color="purple"
+      height={100}
+      width={100}
+      timeout={3000}
+    />
+  );
+}
